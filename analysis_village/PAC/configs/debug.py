6c90dc03f1f13d6b8c5ac8b844de8b429445fd00
nathanielerowe <70993723+nathanielerowe@users.noreply.github.com>
2026-03-16 16:03:22 -0500

    Delete analysis_village/PAC directory

--- analysis_village/PAC/configs/debug.py
+++
@@ -1,5 +0,0 @@
-from analysis_village.PAC.makedf import *
-from preprocess.preprocess import Script
-
-DFS = [make_PAC_nuwgtdf]
-NAMES = ["wgt"]
--- analysis_village/PAC/configs/no_cuts_rewgt.py
+++
@@ -1,5 +0,0 @@
-from analysis_village.PAC.makedf import *
-from preprocess.preprocess import Script
-
-DFS = [make_pandora_no_cuts_df, make_PAC_nudf, make_hdrdf, make_PAC_nuwgtdf]
-NAMES = ["evt", "mcnu", "hdr", "wgt"]
