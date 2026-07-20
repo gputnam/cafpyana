84c7c55172e041ba89eef9a703ec1a8f4edc65a0
nathanielerowe <70993723+nathanielerowe@users.noreply.github.com>
2026-07-19 23:01:33 -0500

    Reorder parameters in load_one function

--- analysis_village/gump/loaddf.py
+++ analysis_village/gump/loaddf.py
@@ -281,8 +281,8 @@ def _write_cache(cache_file, df, match, pot):
 def load_one(fname, idf,
     detector=None, # One of SBND, ICARUS, ICARUS Run4
     include_syst=True, nuniv=100, spline=False, xsec_univ=False, xsec_spline=False,# systematic handling
-    reweight_aFF=False, pot_univ=False, pot_spline=False,
-    flux_univ=True, sep_flux_univ=False, detvar_spline=False,
+    reweight_aFF=False, pot_univ=False, flux_univ=True, sep_flux_univ=False,
+    pot_spline=False, detvar_spline=False,
     load_truth=True, load_crt=False, match_Enu=True, # load extra information
     offbeampot=False, # POT handling
     preselection=None, # apply preselection cut
