691b894858d80dbc89aba3c8eb10f974cb53fcba
Gray Putnam <gputnam@fnal.gov>
2026-08-18 12:44:06 -0500

    Variable naming bug fix.

--- analysis_village/gump/loaddf.py
+++ analysis_village/gump/loaddf.py
@@ -935,7 +935,7 @@ def load_one(fname, idf,
         for d in ["SBND", "ICARUS Run2", "ICARUS Run4"]:
             col_str = f"multisigma_{d.replace(' ', '')}_POT"
             multisigma_cols.append(col_str)
-            if det == d:
+            if detector == d:
                 skim[f"{col_str}"] = [list(pot_syst.values()) for _ in range(len(wgt))]
             else:
                 skim[f"{col_str}"] = [[1.0]*7 for _ in range(len(wgt))]
@@ -1079,8 +1079,8 @@ def load_one(fname, idf,
             else:
                 col_str = s
 
-            # allow for f 
-            if det.replace(' ', '') in fs[0]:
+            # allow for f
+            if detector.replace(' ', '') in fs[0]:
                 s_df = rw.apply_map(mrg, fs, s)
                 mrg[col_str] = s_df
             elif not col_str in mrg.columns:
